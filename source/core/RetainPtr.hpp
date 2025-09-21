ff57d8c8a44d0ff9db1824c8edb3775653143b4a
EddieBreeg <eddiebreeg0@protonmail.com>
2025-09-21 12:36:15 +0200

    Added RetainPtr implementation

---
+++ source/core/RetainPtr.hpp
@@ -0,0 +1,235 @@
+#pragma once
+
+#include <PCH.hpp>
+
+namespace brk {
+	/**
+	 * Retain action: specifies the reference count should be increment when storing a
+	 * new pointer
+	 */
+	struct Retain_t
+	{};
+	/**
+	 * Retain action: specifies the reference count should be left alone when storing
+	 * new pointer
+	 */
+	struct Adopt_t
+	{};
+
+	static constexpr Retain_t s_Retain;
+	static constexpr Adopt_t s_Adopt;
+
+	/**
+	 * Trait class which implements the necessary APIs for a given ref counter class
+	 * \tparam T: The ref counted class
+	 * \details A valid retain traits class must have at least 3 static functions:
+	 * Increment, Decrement and GetCount. Additionally, it can also provide a static
+	 * DefaultAction value, to specify which should be the default behaviour when creating
+	 * a new RetainPtr. If not specified, s_Adopt will be used.
+	 * It must also declare a public static const value named DefaultAction of type either
+	 * Retain_t or Adopt_t. This value specifies the default behaviour when storing a new
+	 * pointer in a RetainPtr object
+	 */
+	// template <class T>
+	// struct RetainTraits;
+
+	template <class TraitType, class T>
+	concept RetainTraits = requires(T * ptr)
+	{
+		{ TraitType::Increment(ptr) };
+		{ TraitType::Decrement(ptr) };
+		{ TraitType::GetCount((const T*)ptr) }->std::convertible_to<uint32>;
+	};
+
+	template <class T>
+	struct DefaultRetainTraits
+	{
+		static constexpr Retain_t DefaultAction{};
+		static void Increment(T* ptr) noexcept { ++ptr->m_RefCount; }
+		static void Decrement(T* ptr) noexcept
+		{
+			if (!--ptr->m_RefCount)
+				delete ptr;
+		}
+		static uint32 GetCount(const T* ptr) noexcept { return ptr->m_RefCount; }
+	};
+
+	/**
+	 *  A smart pointer with intrusive reference counting
+	 * \tparam T: The type of object pointed at
+	 * \tparam Traits: The retain traits which defines all relevant operations on the
+	 * reference counter (Increment, Decrement and GetCount) as well as DefaultAction
+	 */
+	template <class T, RetainTraits<T> Traits = DefaultRetainTraits<T>>
+	class RetainPtr
+	{
+	public:
+		using TraitsType = Traits;
+		using CounterType = decltype(TraitsType::GetCount(std::declval<const T*>()));
+
+		/**
+		 * Initializes the stored pointer to nullptr
+		 */
+		constexpr RetainPtr() = default;
+		/**
+		 * Constructs an object which stores the specified pointer, without incrementing
+		 * the reference count
+		 */
+		RetainPtr(T* ptr, Adopt_t);
+		/**
+		 * Stores the specified pointer, and increments its reference count if ptr is not
+		 * nullptr
+		 */
+		RetainPtr(T* ptr, Retain_t);
+		/**
+		 * Stores the specified pointer, and derives the ref count behaviour from
+		 * Traits::DefaultAction
+		 */
+		explicit RetainPtr(T* ptr);
+
+		/** Copies the stored pointer and increments the ref count if not null */
+		RetainPtr(const RetainPtr& other);
+		RetainPtr(RetainPtr&& other) noexcept;
+
+		/** Effectively calls Reset(ptr, other.ptr) */
+		RetainPtr& operator=(const RetainPtr& other);
+		/** Effectively calls std::swap(ptr, other.ptr) */
+		RetainPtr& operator=(RetainPtr&& other) noexcept;
+
+		/** If the stored pointer is not null, decrements the reference counter */
+		~RetainPtr();
+
+		/** Replaces the stored pointer with the specified value, without incrementing the
+		 * reference counter. If the previously stored pointer was not nullptr, the
+		 * reference count is decremented */
+		void Reset(T* ptr, Adopt_t);
+		/** Replaces the stored pointer with the specified value, and increments the
+		 * reference counter if said value is not nullptr. . If the previously stored
+		 * pointer was not nullptr, the reference count is decremented */
+		void Reset(T* ptr, Retain_t);
+		/** Calls Reset(ptr, Traits::DefaultAction) */
+		void Reset(T* ptr = nullptr);
+
+		[[nodiscard]] T* Get() noexcept { return m_Ptr; }
+		[[nodiscard]] const T* Get() const noexcept { return m_Ptr; }
+		T* operator->() noexcept { return m_Ptr; }
+		const T* operator->() const noexcept { return m_Ptr; }
+		[[nodiscard]] T& operator*() { return *m_Ptr; }
+		[[nodiscard]] const T& operator*() const { return *m_Ptr; }
+
+		[[nodiscard]] constexpr operator bool() const noexcept { return bool(m_Ptr); }
+
+		/** Returns the current value of the reference counter. Only thread safe is the
+		 * counter itself is atomic */
+		[[nodiscard]] CounterType GetCount() const;
+
+		/** Replaces the stored pointer with nullptr without decrementing the reference
+		 * count, and returns the old value */
+		T* Release() noexcept { return std::exchange(m_Ptr, nullptr); }
+
+		template <class U = T, class UTraits = TraitsType>
+		[[nodiscard]] constexpr bool operator==(const RetainPtr<U, UTraits>& other) const noexcept
+		{
+			return m_Ptr == other.m_Ptr;
+		}
+
+		template <class U = T, class UTraits = TraitsType>
+		[[nodiscard]] constexpr bool operator!=(const RetainPtr<U, UTraits>& other) const noexcept
+		{
+			return m_Ptr != other.m_Ptr;
+		}
+
+		void Swap(RetainPtr& other) noexcept
+		{
+			std::swap(m_Ptr, other.m_Ptr);
+		}
+
+	private:
+		T* m_Ptr = nullptr;
+	};
+
+	template <class T, RetainTraits<T> Traits>
+	inline RetainPtr<T, Traits>::RetainPtr(T* ptr, Adopt_t)
+		: m_Ptr{ ptr }
+	{}
+
+	template <class T, RetainTraits<T> Traits>
+	inline RetainPtr<T, Traits>::RetainPtr(T* ptr, Retain_t)
+		: m_Ptr{ ptr }
+	{
+		if (m_Ptr)
+			Traits::Increment(m_Ptr);
+	}
+
+	template <class T, RetainTraits<T> Traits>
+	inline RetainPtr<T, Traits>::RetainPtr(T* ptr)
+		: RetainPtr{ ptr, Traits::DefaultAction }
+	{}
+
+	template <class T, RetainTraits<T> Traits>
+	inline RetainPtr<T, Traits>::~RetainPtr()
+	{
+		Reset();
+	}
+
+	template <class T, RetainTraits<T> Traits>
+	inline RetainPtr<T, Traits>::RetainPtr(const RetainPtr& other)
+		: m_Ptr{ other.m_Ptr }
+	{
+		if (m_Ptr)
+			Traits::Increment(m_Ptr);
+	}
+
+	template <class T, RetainTraits<T> Traits>
+	inline RetainPtr<T, Traits>::RetainPtr(RetainPtr&& other) noexcept
+		: m_Ptr{ other.m_Ptr }
+	{
+		other.m_Ptr = nullptr;
+	}
+
+	template <class T, RetainTraits<T> Traits>
+	inline RetainPtr<T, Traits>& RetainPtr<T, Traits>::operator=(const RetainPtr& other)
+	{
+		Reset(other.m_Ptr, s_Retain);
+		return *this;
+	}
+
+	template <class T, RetainTraits<T> Traits>
+	inline RetainPtr<T, Traits>& RetainPtr<T, Traits>::operator=(RetainPtr&& other) noexcept
+	{
+		std::swap(m_Ptr, other.m_Ptr);
+		return *this;
+	}
+
+	template <class T, RetainTraits<T> Traits>
+	inline void RetainPtr<T, Traits>::Reset(T* ptr, Adopt_t)
+	{
+		if (m_Ptr)
+			Traits::Decrement(m_Ptr);
+		m_Ptr = ptr;
+	}
+
+	template <class T, RetainTraits<T> Traits>
+	inline void RetainPtr<T, Traits>::Reset(T* ptr, Retain_t)
+	{
+		if (m_Ptr)
+			Traits::Decrement(m_Ptr);
+		if ((m_Ptr = ptr))
+			Traits::Increment(m_Ptr);
+	}
+
+	template <class T, RetainTraits<T> Traits>
+	inline void RetainPtr<T, Traits>::Reset(T* ptr)
+	{
+		Reset(ptr, Traits::DefaultAction);
+	}
+
+	template <class T, RetainTraits<T> Traits>
+	inline RetainPtr<T, Traits>::CounterType brk::RetainPtr<T, Traits>::GetCount() const
+	{
+		using TRes = decltype(Traits::GetCount(m_Ptr));
+		if (!m_Ptr)
+			return TRes{ 0 };
+		return Traits::GetCount(m_Ptr);
+	}
+} // namespace brk
---
+++ tests/RetainPtrTests.cpp
@@ -0,0 +1,289 @@
+#include <catch2/catch_test_macros.hpp>
+#include <core/RetainPtr.hpp>
+
+#define RETAIN_PTR_TEST(name) TEST_CASE(name, "[retain_ptr]")
+
+namespace brk::retain_ptr::ut {
+	struct S1
+	{
+		uint32 m_RefCount = 0;
+		bool m_Deleted = false;
+
+		void operator delete(void* ptr) { static_cast<S1*>(ptr)->m_Deleted = true; }
+
+		struct RetainTraits
+		{};
+	};
+
+	RETAIN_PTR_TEST("GetCount")
+	{
+		{
+			const S1 s;
+			CHECK(DefaultRetainTraits<S1>::GetCount(&s) == 0);
+		}
+		{
+			const S1 s{ 1 };
+			CHECK(DefaultRetainTraits<S1>::GetCount(&s) == 1);
+		}
+	}
+
+	RETAIN_PTR_TEST("Increment")
+	{
+		S1 s;
+		DefaultRetainTraits<S1>::Increment(&s);
+		CHECK(s.m_RefCount == 1);
+	}
+
+	RETAIN_PTR_TEST("Decrement")
+	{
+		S1 s{ 2 };
+		DefaultRetainTraits<S1>::Decrement(&s);
+		CHECK(s.m_RefCount == 1);
+	}
+
+	RETAIN_PTR_TEST("Decrement and Delete")
+	{
+		S1 s{ 1 };
+		DefaultRetainTraits<S1>::Decrement(&s);
+		CHECK(s.m_RefCount == 0);
+		CHECK(s.m_Deleted);
+	}
+
+	RETAIN_PTR_TEST("Empty RetainPtr")
+	{
+		RetainPtr<S1> ptr;
+		CHECK(!ptr);
+	}
+
+	RETAIN_PTR_TEST("Adopt")
+	{
+		S1 s;
+		RetainPtr<S1> ptr{ &s, Adopt_t{} };
+		REQUIRE(!s.m_Deleted);
+		CHECK(ptr.GetCount() == 0);
+	}
+
+	RETAIN_PTR_TEST("Retain")
+	{
+		S1 s;
+		RetainPtr<S1> ptr{ &s, Retain_t{} };
+		REQUIRE(!s.m_Deleted);
+		CHECK(ptr.GetCount() == 1);
+	}
+
+	RETAIN_PTR_TEST("Default Action")
+	{
+		S1 s;
+		RetainPtr<S1> ptr{ &s };
+		REQUIRE(!s.m_Deleted);
+		CHECK(ptr.GetCount() == 1);
+	}
+
+	RETAIN_PTR_TEST("Destroy")
+	{
+		S1 s;
+		{
+			RetainPtr<S1> ptr{ &s };
+		}
+		CHECK(s.m_RefCount == 0);
+		CHECK(s.m_Deleted);
+	}
+
+	RETAIN_PTR_TEST("Reset Adopt")
+	{
+		SECTION("nullptr")
+		{
+			S1 s1;
+			RetainPtr<S1> ptr{ &s1 };
+			REQUIRE(ptr.GetCount() == 1);
+			ptr.Reset(nullptr, Adopt_t{});
+
+			CHECK(s1.m_RefCount == 0);
+			CHECK(s1.m_Deleted);
+		}
+		SECTION("!nullptr")
+		{
+			S1 s1;
+			RetainPtr<S1> ptr{ &s1 };
+			REQUIRE(ptr.GetCount() == 1);
+			S1 s2;
+			ptr.Reset(&s2, Adopt_t{});
+
+			CHECK(s1.m_RefCount == 0);
+			CHECK(s1.m_Deleted);
+			CHECK(s2.m_RefCount == 0);
+			CHECK_FALSE(s2.m_Deleted);
+		}
+	}
+
+	RETAIN_PTR_TEST("Reset Retain")
+	{
+		SECTION("nullptr")
+		{
+			S1 s1;
+			RetainPtr<S1> ptr{ &s1 };
+			REQUIRE(ptr.GetCount() == 1);
+			ptr.Reset(nullptr, Retain_t{});
+
+			CHECK(s1.m_RefCount == 0);
+			CHECK(s1.m_Deleted);
+		}
+		SECTION("!nullptr")
+		{
+			S1 s1;
+			RetainPtr<S1> ptr{ &s1 };
+			REQUIRE(ptr.GetCount() == 1);
+			S1 s2;
+			ptr.Reset(&s2, Retain_t{});
+
+			CHECK(s1.m_RefCount == 0);
+			CHECK(s1.m_Deleted);
+			CHECK(s2.m_RefCount == 1);
+			CHECK_FALSE(s2.m_Deleted);
+		}
+	}
+
+	RETAIN_PTR_TEST("Reset Default")
+	{
+		SECTION("nullptr")
+		{
+			S1 s1;
+			RetainPtr<S1> ptr{ &s1 };
+			REQUIRE(ptr.GetCount() == 1);
+			ptr.Reset();
+
+			CHECK(s1.m_RefCount == 0);
+			CHECK(s1.m_Deleted);
+		}
+		SECTION("!nullptr")
+		{
+			S1 s1;
+			RetainPtr<S1> ptr{ &s1 };
+			REQUIRE(ptr.GetCount() == 1);
+			S1 s2;
+			ptr.Reset(&s2);
+
+			CHECK(s1.m_RefCount == 0);
+			CHECK(s1.m_Deleted);
+			CHECK(s2.m_RefCount == 1);
+			CHECK_FALSE(s2.m_Deleted);
+		}
+	}
+
+	RETAIN_PTR_TEST("Copy")
+	{
+		SECTION("Copy Construction")
+		{
+			S1 s;
+			const RetainPtr ptr{ &s };
+			REQUIRE(s.m_RefCount == 1);
+			{
+				const RetainPtr ptr2{ ptr };
+				CHECK(ptr == ptr2);
+				CHECK(ptr.GetCount() == 2);
+				CHECK(ptr2.GetCount() == 2);
+			}
+			CHECK(s.m_RefCount == 1);
+			CHECK_FALSE(s.m_Deleted);
+		}
+		SECTION("Copy Assignment")
+		{
+			S1 s1;
+			S1 s2;
+			RetainPtr ptr{ &s1 };
+
+			REQUIRE(s1.m_RefCount == 1);
+			{
+				const RetainPtr ptr2{ &s2 };
+				REQUIRE(s2.m_RefCount == 1);
+				ptr = ptr2;
+
+				CHECK(s1.m_RefCount == 0);
+				CHECK(s1.m_Deleted);
+
+				CHECK(ptr == ptr2);
+				CHECK(s2.m_RefCount == 2);
+			}
+			CHECK(s2.m_RefCount == 1);
+			CHECK_FALSE(s2.m_Deleted);
+		}
+	}
+
+	RETAIN_PTR_TEST("Move")
+	{
+		SECTION("Move Construction")
+		{
+			S1 s;
+			RetainPtr ptr{ &s };
+			REQUIRE(s.m_RefCount == 1);
+			{
+				const RetainPtr ptr2{ std::move(ptr) };
+				CHECK_FALSE(ptr);
+				CHECK(s.m_RefCount == 1);
+				CHECK_FALSE(s.m_Deleted);
+			}
+			CHECK(s.m_RefCount == 0);
+			CHECK(s.m_Deleted);
+		}
+		SECTION("Move Assignment")
+		{
+			S1 s1, s2;
+			RetainPtr ptr{ &s1 };
+			REQUIRE(s1.m_RefCount == 1);
+			{
+				RetainPtr ptr2{ &s2 };
+				REQUIRE(s2.m_RefCount == 1);
+
+				ptr = std::move(ptr2);
+				CHECK(ptr.Get() == &s2);
+				CHECK(ptr2.Get() == &s1);
+
+				CHECK(s1.m_RefCount == 1);
+				CHECK(s2.m_RefCount == 1);
+
+				CHECK_FALSE(s1.m_Deleted);
+				CHECK_FALSE(s2.m_Deleted);
+			}
+			CHECK(s1.m_RefCount == 0);
+			CHECK(s1.m_Deleted);
+		}
+	}
+
+	RETAIN_PTR_TEST("Release")
+	{
+		S1 s;
+		RetainPtr ptr{ &s };
+		CHECK(ptr.Release() == &s);
+		CHECK(s.m_RefCount == 1);
+		CHECK_FALSE(s.m_Deleted);
+		CHECK_FALSE(ptr);
+	}
+
+	RETAIN_PTR_TEST("Swap")
+	{
+		S1 s, s2;
+		RetainPtr ptr{ &s };
+		RetainPtr ptr2{ &s2 };
+		ptr.Swap(ptr2);
+
+		CHECK(s.m_RefCount == 1);
+		CHECK(s2.m_RefCount == 1);
+		CHECK_FALSE(s.m_Deleted);
+		CHECK_FALSE(s2.m_Deleted);
+		CHECK(ptr.Get() == &s2);
+		CHECK(ptr2.Get() == &s);
+	}
+
+	RETAIN_PTR_TEST("Dereference")
+	{
+		S1 s;
+		RetainPtr<S1> ptr{ &s };
+		CHECK(ptr->m_RefCount == 1);
+		CHECK_FALSE(ptr->m_Deleted);
+
+		CHECK((*ptr).m_RefCount == 1);
+		CHECK_FALSE((*ptr).m_Deleted);
+	}
+} // namespace brk::retain_ptr::ut
+
+#undef RETAIN_PTR_TEST
