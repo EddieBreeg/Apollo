e7e337cbfc743ba1a588ac6ec7b94b2ea8bc0c6b
EddieBreeg <eddiebreeg0@protonmail.com>
2025-11-03 20:42:08 +0100

    Removed python build script
    Now superseeded by cmake workflows

--- Build.py
+++
@@ -1,9 +0,0 @@
-from os import system
-
-def Build(presetName: str):
-	cmd = "cmake --build --preset %s" % presetName
-	system(cmd)
-
-if __name__ == '__main__':
-	Build("msvc_debug")
-	Build("ninja_debug")
