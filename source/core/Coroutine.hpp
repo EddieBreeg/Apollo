3b422a07ab228c2a55e570c7a039ac6cfe235bf0
EddieBreeg <eddiebreeg0@protonmail.com>
2026-03-09 16:16:27 +0100

    Basic coroutine support

---
+++ source/core/Coroutine.hpp
@@ -0,0 +1,133 @@
+#pragma once
+
+#include <PCH.hpp>
+#include <coroutine>
+
+#include "Assert.hpp"
+
+namespace apollo::coro {
+	template <class To, class From>
+	[[nodiscard]] std::coroutine_handle<To> StaticHandleCast(std::coroutine_handle<From> h) noexcept
+		requires(requires(From* x) { static_cast<To*>(x); })
+	{
+		return std::coroutine_handle<To>::from_address(h.address());
+	}
+
+	template <class P = void>
+	class Coroutine
+	{
+	public:
+		using promise_type = P;
+		using HandleType = std::coroutine_handle<P>;
+
+		constexpr Coroutine() noexcept = default;
+		explicit Coroutine(HandleType handle) noexcept
+			: m_Handle(handle)
+		{}
+		constexpr Coroutine(Coroutine&& other) noexcept
+			: m_Handle(other.m_Handle)
+		{
+			other.m_Handle = nullptr;
+		}
+		constexpr Coroutine& operator=(Coroutine&& other) noexcept
+		{
+			Swap(other);
+			return *this;
+		}
+
+		template <class O>
+		constexpr Coroutine(Coroutine<O>&& other) noexcept
+			requires(!std::is_same_v<O, P> && requires(O * x) { static_cast<P*>(x); })
+			: m_Handle(StaticHandleCast<P>(other.m_Handle))
+		{
+			other.m_Handle = nullptr;
+		}
+
+		[[nodiscard]] P* operator->() noexcept { return &m_Handle.promise(); }
+		[[nodiscard]] const P* operator->() const noexcept { return &m_Handle.promise(); }
+
+		/*
+		 * If the internal handle points to a currently suspended coroutine, resumes it. Otherwise
+		 * returns true immediately.
+		 * \returns true if the underlying coroutine is null or has reached its final suspension
+		 * point, false otherwise.
+		 */
+		bool operator()()
+		{
+			APOLLO_ASSERT((bool)m_Handle, "Tried to resume a null coroutine");
+
+			if (m_Handle.done())
+				return true;
+
+			m_Handle.resume();
+			return m_Handle.done();
+		}
+
+		[[nodiscard]] constexpr bool IsDone() const noexcept { return m_Handle && m_Handle.done(); }
+		[[nodiscard]] constexpr operator bool() const noexcept { return (bool)m_Handle; }
+
+		[[nodiscard]] constexpr bool operator==(HandleType handle) const noexcept
+		{
+			return m_Handle == handle;
+		}
+		[[nodiscard]] constexpr bool operator!=(HandleType handle) const noexcept
+		{
+			return m_Handle != handle;
+		}
+
+		constexpr void Reset(HandleType handle = nullptr) noexcept
+		{
+			if (m_Handle)
+				m_Handle.destroy();
+			m_Handle = handle;
+		}
+
+		[[nodiscard("Discarding the returned handle would cause a leak")]] constexpr HandleType Release(
+			HandleType other = nullptr) noexcept
+		{
+			auto temp = m_Handle;
+			m_Handle = other;
+			return temp;
+		}
+
+		constexpr void Swap(Coroutine& other) noexcept { std::swap(m_Handle, other.m_Handle); }
+
+		constexpr ~Coroutine()
+		{
+			if (m_Handle)
+				m_Handle.destroy();
+		}
+
+	protected:
+		template <class O>
+		friend class Coroutine;
+		HandleType m_Handle;
+	};
+
+	template <bool EagerStart = false>
+	struct NoopPromise
+	{
+		Coroutine<NoopPromise> get_return_object() noexcept
+		{
+			return Coroutine{ std::coroutine_handle<NoopPromise>::from_promise(*this) };
+		}
+		template <class To>
+		Coroutine<To> get_return_object() noexcept
+		{
+			return Coroutine<To>{ std::coroutine_handle<To>::from_promise(*this) };
+		}
+
+		void unhandled_exception() const noexcept {}
+
+		using InitialSuspendType = std::
+			conditional_t<EagerStart, std::suspend_never, std::suspend_always>;
+		InitialSuspendType initial_suspend() const noexcept { return {}; }
+		std::suspend_always final_suspend() const noexcept { return {}; }
+
+		template <class T>
+		std::suspend_always yield_value(T&&) const noexcept
+		{
+			return {};
+		}
+	};
+} // namespace apollo::coro
---
+++ tests/CoroutineTests.cpp
@@ -0,0 +1,125 @@
+#include <catch2/catch_test_macros.hpp>
+#include <core/Coroutine.hpp>
+
+#define CORO_TEST(name) TEST_CASE(name, "[coroutine]")
+
+namespace {
+	template <class T, bool EagerStart = false>
+	struct P1 : public apollo::coro::NoopPromise<EagerStart>
+	{
+		T m_Res{};
+
+		void return_value(T val) { m_Res = val; }
+		std::suspend_always yield_value(T val)
+		{
+			m_Res = val;
+			return {};
+		}
+	};
+
+	apollo::coro::Coroutine<P1<int>> C1()
+	{
+		co_yield 1;
+		co_return 2;
+	}
+	apollo::coro::Coroutine<P1<int>> C2(int val)
+	{
+		co_await std::suspend_always{};
+		co_return val + 1;
+	}
+} // namespace
+
+namespace apollo::coro::ut {
+	CORO_TEST("Null coroutine to bool")
+	{
+		using C = Coroutine<>;
+		static_assert(!C{});
+	}
+
+	CORO_TEST("Call on null coroutine")
+	{
+		CHECK_THROWS_AS(Coroutine<>{}(), _internal::BreakException);
+	}
+
+	CORO_TEST("No-op coroutine")
+	{
+		Coroutine<> noop{ std::noop_coroutine() };
+		CHECK(noop);
+		CHECK(!noop());
+	}
+
+	CORO_TEST("Yield 1, return 2")
+	{
+		auto coro = C1();
+		REQUIRE(!coro.IsDone());
+		coro();
+		CHECK(coro->m_Res == 1);
+		coro();
+		CHECK(coro->m_Res == 2);
+		CHECK(coro.IsDone());
+	}
+
+	CORO_TEST("Await then return 1")
+	{
+		auto coro = C2(0);
+		REQUIRE(coro->m_Res == 0);
+		coro();
+		CHECK(coro->m_Res == 0);
+		REQUIRE(coro());
+		CHECK(coro->m_Res == 1);
+	}
+
+	CORO_TEST("Reset to null")
+	{
+		Coroutine<> coro = C1();
+		REQUIRE(!coro.IsDone());
+		coro.Reset();
+		CHECK(!coro);
+	}
+
+	CORO_TEST("Reset to noop")
+	{
+		Coroutine<> coro = C1();
+		REQUIRE(!coro.IsDone());
+		const auto handle = std::noop_coroutine();
+		coro.Reset(handle);
+		CHECK(coro);
+		CHECK((coro == handle));
+	}
+
+	CORO_TEST("Swap")
+	{
+		auto c1 = C1();
+		c1();
+		auto c2 = C1();
+		REQUIRE(c1->m_Res == 1);
+		REQUIRE(c2->m_Res == 0);
+		
+		c1.Swap(c2);
+		REQUIRE(c1->m_Res == 0);
+		REQUIRE(c2->m_Res == 1);
+	}
+	
+	CORO_TEST("Move assign")
+	{
+		auto c1 = C1();
+		c1();
+		auto c2 = C1();
+		REQUIRE(c1->m_Res == 1);
+		REQUIRE(c2->m_Res == 0);
+		
+		c1 = std::move(c2);
+		REQUIRE(c1->m_Res == 0);
+		REQUIRE(c2->m_Res == 1);
+	}
+
+	CORO_TEST("Release")
+	{
+		auto c1 = C1();
+		auto& p = *c1.operator->();
+		auto handle = std::coroutine_handle<P1<int>>::from_promise(p);
+		CHECK((c1.Release() == handle));
+		handle.destroy();
+		CHECK(!c1);
+	}
+} // namespace apollo::coro::ut
